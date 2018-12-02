6db8822d87f0d5189f00543c97196f0778f228d0
Alexandr <alexandr.agress1on@gmail.com>
2018-12-02 15:43:18 +0300

    - удалил еще лишний код

--- app/src/main/java/com/example/alexandr/megaquiz/start/StartView.java
+++ app/src/main/java/com/example/alexandr/megaquiz/start/StartView.java
@@ -164,24 +164,4 @@ public class StartView extends AppCompatActivity implements StartContract.View, 
         }
 
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        /*
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-        */
-        switch (item.getItemId()) {
-            case R.id.action_settings : return true;
-        }
-        return super.onOptionsItemSelected(item);
-
-    }
 }
